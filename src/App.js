cb23be7f8f95af9a8c9b80b7caec8ef3e20e9515
sonangrai <shonahangrae@gmail.com>
2021-02-22 18:09:10 +0545

    added the helmet for title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./sass/main.scss";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
+
 import Sidebar from "./components/layouts/Sidebar";
 import Home from "./components/pages/Home";
 import Header from "./components/layouts/Header";
@@ -8,6 +10,9 @@ function App() {
   return (
     <>
       <Router>
+        <Helmet>
+          <title>NCHM</title>
+        </Helmet>
         <div className="app__container">
           <Sidebar />
           <div className="page__container">
